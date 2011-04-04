08b6f52394b6e75929352f612f9229d19dd6d5b6
Joe Yates <joe.g.yates@gmail.com>
2011-04-04 21:38:56 +0100

    Bumped version to 0.0.14

--- lib/rake/builder/version.rb
+++ lib/rake/builder/version.rb
@@ -3,9 +3,9 @@ module Rake
   class Builder
 
     module VERSION #:nodoc:
-      MAJOR = 0
-      MINOR = 0
-      TINY  = 13
+      MAJOR =  0
+      MINOR =  0
+      TINY  = 14
 
       STRING = [ MAJOR, MINOR, TINY ].join('.')
     end
