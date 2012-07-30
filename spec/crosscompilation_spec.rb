e55a74406aa048468496224cc18b8f01fcd50b72
Benjamin Hase <benjamin.hase@web.de>
2012-07-30 21:08:57 +0200

    cleanup crosscompilation_spec.rb

--- spec/crosscompilation_spec.rb
+++ spec/crosscompilation_spec.rb
@@ -4,10 +4,6 @@ describe 'when building a cross compilation project' do
 
   include RakeBuilderHelper
 
-  before( :all ) do
-    #@test_output_file = Rake::Path.expand_with_root( 'rake-c-testfile.txt', RakeBuilderHelper::SPEC_PATH )
-  end
-
   before( :each ) do
     Rake::Task.clear
     @project = c_task( :executable )
@@ -17,12 +13,6 @@ describe 'when building a cross compilation project' do
         build_commands.to_s
       end
     end
-  end
-
-  after( :each ) do
-    #Rake::Task[ 'clean' ].invoke
-    #`rm -f #{ @test_output_file }`
-    #`rm -f '#{ @project.local_config }'`
   end
 
   it "respects the cross compilation prefix" do
