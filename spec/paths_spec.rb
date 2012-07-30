5cd2d64ffdb821c11a3edeb46dedc92f6b611c70
Benjamin Hase <benjamin.hase@web.de>
2012-07-30 21:39:57 +0200

    add test case for objects_path

--- spec/paths_spec.rb
+++ spec/paths_spec.rb
@@ -4,6 +4,9 @@ PATHS_SPEC_PATH = File.expand_path( File.dirname(__FILE__) )
 
 describe 'when creating tasks' do
 
+  include RakeBuilderHelper
+  include FileUtils
+
   before( :each ) do
     Rake::Task.clear
   end
@@ -15,5 +18,14 @@ describe 'when creating tasks' do
     builder.rakefile_path.should == PATHS_SPEC_PATH
   end
 
+  it 'puts everything in objects_path' do
+    builder = Rake::Builder.new do |builder|
+      builder.source_search_paths = [ 'cpp_project' ]
+      builder.objects_path = 'test_directory'
+    end
+    path = File.join(PATHS_SPEC_PATH, 'test_directory')
+    File.dirname(builder.target).should == path
+    builder.generated_files.map{ |d| File.dirname(d).should == path }
+  end
 end
 
