9fd865b5862acf46404cb95aeb4c6fb939af6314
Joe Yates <joe.g.yates@gmail.com>
2013-01-20 16:39:44 +0100

    Update example project

--- examples/04_zlib/src/main.c
+++ examples/04_zlib/src/main.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
-int main( int argc, char *argv[] ) {
+int main(int argc, char *argv[]) {
   gzFile gzip;
 
   gzip = gzopen("rake.gz", "w");
-  gzprintf( gzip, "Hello!" );
-  gzclose( gzip );
+  gzprintf(gzip, "Hello!");
+  gzclose(gzip);
 
   return 0;
 }
+
