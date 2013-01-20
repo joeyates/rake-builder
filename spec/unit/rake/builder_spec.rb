fd8ca7361b5bfc3b3293d57269e1e2975ca43ec1
Joe Yates <joe.g.yates@gmail.com>
2013-01-20 14:29:17 +0100

    Example for version use by builder

--- spec/unit/rake/builder_spec.rb
+++ spec/unit/rake/builder_spec.rb
@@ -20,6 +20,14 @@ describe Rake::Builder do
       }.to raise_error(RuntimeError, 'Please supply a project_title parameter')
     end
 
+    it 'fails if Version fails' do
+      Rake::Builder::Version.stub(:new).and_raise('foo')
+
+      expect {
+        Rake::Builder.create_autoconf('foo', 'bar', 'baz')
+      }.to raise_error(RuntimeError, 'foo')
+    end
+
     it 'fails if configure.ac exists' do
       File.should_receive(:exist?).with('configure.ac').and_return(true)
 
