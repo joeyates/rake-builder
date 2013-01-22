25437ca3e1ad832b8487bb3800b85ad4c4bd5039
Joe Yates <joe.g.yates@gmail.com>
2013-01-22 10:57:28 +0100

    Add specs for makefile presenter

--- lib/rake/builder/presenters/makefile/builder_presenter.rb
+++ lib/rake/builder/presenters/makefile/builder_presenter.rb
@@ -4,57 +4,17 @@ module Rake::Builder::Presenters::Makefile
   class BuilderPresenter
     def initialize(builder)
       @builder = builder
+      check_target_type
     end
 
     def to_s
-      objects      = @builder.object_files.collect { |f| f.sub(@builder.objects_path, '$(OBJECT_DIR)') }
-      objects_list = objects.join( ' ' )
-      case @builder.target_type
-      when :executable
-        target_name = 'EXECUTABLE_TARGET'
-        target_ref  = "$(#{target_name})"
-        target_actions =
-"	$(LINKER) $(LINK_FLAGS) -o #{target_ref} $(OBJECTS)
-"
-      when :static_library
-        target_name = 'LIB_TARGET'
-        target_ref  = "$(#{target_name})"
-        target_actions =
-"	rm -f #{target_ref}
-	ar -cq #{target_ref} $(OBJECTS)
-	ranlib #{target_ref}
-"
-      when :shared_library
-        target_name = 'LIB_TARGET'
-        target_ref  = "$(#{target_name})"
-        target_actions =
-"	$(LINKER) -shared -o #{target_ref} $(OBJECTS) $(LINK_FLAGS)
-"
-      end
+      check_target_type
 
-      variables = <<EOT
-COMPILER       = #{@builder.compiler}
-COMPILER_FLAGS = #{@builder.compiler_flags}
-LINKER         = #{@builder.linker}
-LINK_FLAGS     = #{@builder.link_flags}
-OBJECT_DIR     = #{@builder.objects_path}
-OBJECTS        = #{objects_list}
-#{ target_name } = #{@builder.target}
-EOT
-      rules = <<EOT
-
-all: #{target_ref}
-
-clean:
-	rm -f $(OBJECTS)
-	rm -f #{target_ref}
-
-#{target_ref}: $(OBJECTS)
-#{target_actions}
-EOT
+      variables = main_variables
+      rules     = main_rules
 
       source_groups = group_files_by_path(@builder.source_files)
-      source_groups.each.with_index do | gp, i |
+      source_groups.each.with_index do |gp, i|
         variables << "SOURCE_#{i + 1} = #{gp[0]}\n"
         rules     << <<EOT
 
@@ -71,11 +31,84 @@ EOT
       end
     end
 
-    def group_files_by_path( files )
-      files.group_by do | f |
-        m = f.match( /(.*?)?\/?([^\/]+)$/ )
-        m[ 1 ]
+    private
+
+    def target_type
+      @builder.target_type
+    end
+
+    def target_name
+      case target_type
+      when :executable
+        'EXECUTABLE_TARGET'
+      when :static_library
+        'LIB_TARGET'
+      when :shared_library
+        'LIB_TARGET'
       end
+    end
+
+    def target_ref
+      "$(#{target_name})"
+    end
+
+    def target_actions
+      case target_type
+      when :executable
+"	$(LINKER) $(LINK_FLAGS) -o #{target_ref} $(OBJECTS)
+"
+      when :static_library
+"	rm -f #{target_ref}
+	ar -cq #{target_ref} $(OBJECTS)
+	ranlib #{target_ref}
+"
+      when :shared_library
+"	$(LINKER) -shared -o #{target_ref} $(OBJECTS) $(LINK_FLAGS)
+"
+      end
+    end
+
+    def main_variables
+      <<EOT
+COMPILER       = #{@builder.compiler}
+COMPILER_FLAGS = #{@builder.compiler_flags}
+LINKER         = #{@builder.linker}
+LINK_FLAGS     = #{@builder.link_flags}
+OBJECT_DIR     = #{@builder.objects_path}
+OBJECTS        = #{objects_list}
+#{target_name} = #{@builder.target}
+EOT
+    end
+
+    def main_rules
+      <<EOT
+
+all: #{target_ref}
+
+clean:
+	rm -f $(OBJECTS)
+	rm -f #{target_ref}
+
+#{target_ref}: $(OBJECTS)
+#{target_actions}
+EOT
+    end
+
+    def objects_list
+      objects = @builder.object_files.map { |f| f.sub(@builder.objects_path, '$(OBJECT_DIR)') }
+      objects.join(' ')
+    end
+
+    def group_files_by_path(files)
+      files.group_by do |f|
+        m = f.match(/(.*?)?\/?([^\/]+)$/)
+        m[1]
+      end
+    end
+
+    def check_target_type
+      return if [:executable, :shared_library, :static_library].include?(target_type)
+      raise "Unknown build target type '#{target_type}'"
     end
   end
 end
---
+++ spec/unit/rake/builder/presenters/makefile/builder_presenter_spec.rb
@@ -0,0 +1,157 @@
+require 'spec_helper'
+
+describe Rake::Builder::Presenters::Makefile::BuilderPresenter do
+  def self.compiler; 'the_compiler'; end
+  def self.compiler_flags; 'the_compiler_flags'; end
+  def self.linker; 'the_linker'; end
+  def self.link_flags; 'the_link_flags'; end
+  def self.objects_path; '/objects'; end
+
+  let(:executable_target) { 'the_executable' }
+  let(:shared_library_target) { 'libshared.so' }
+  let(:static_library_target) { 'libstatic.a' }
+  let(:label) { 'the_label' }
+  let(:source_paths) { ['/sources'] }
+  let(:source_files) { ['/sources/one.c', '/sources/two.c'] }
+  let(:object_files) { ['/objects/one.o', '/objects/two.o'] }
+  let(:makefile_name) { 'the_makefile' }
+  let(:common_attributes) do
+    {
+      :label => label,
+      :compiler => self.class.compiler,
+      :compiler_flags => self.class.compiler_flags,
+      :linker => self.class.linker,
+      :link_flags => self.class.link_flags,
+      :objects_path => self.class.objects_path,
+      :source_paths => source_paths,
+      :source_files => source_files,
+      :object_files => object_files,
+      :makefile_name => makefile_name,
+    }
+  end
+  let(:executable_attributes) do
+    common_attributes.merge(
+      :target => executable_target,
+      :target_type => :executable,
+    )
+  end
+  let(:static_library_attributes) do
+    common_attributes.merge(
+      :target => static_library_target,
+      :target_type => :static_library,
+    )
+  end
+  let(:shared_library_attributes) do
+    common_attributes.merge(
+      :target => shared_library_target,
+      :target_type => :shared_library,
+    )
+  end
+  
+  let(:executable_builder) { stub('Rake::Builder', executable_attributes) }
+  let(:static_library_builder) { stub('Rake::Builder', static_library_attributes) }
+  let(:shared_library_builder) { stub('Rake::Builder', shared_library_attributes) }
+
+  let(:executable_subject) { Rake::Builder::Presenters::Makefile::BuilderPresenter.new(executable_builder) }
+  let(:static_library_subject) { Rake::Builder::Presenters::Makefile::BuilderPresenter.new(static_library_builder) }
+  let(:shared_library_subject) { Rake::Builder::Presenters::Makefile::BuilderPresenter.new(shared_library_builder) }
+
+  context '.new' do
+    it 'takes one parameter' do
+      expect {
+        Rake::Builder::Presenters::Makefile::BuilderPresenter.new
+      }.to raise_error(ArgumentError, /wrong number of arguments/)
+    end
+
+    it 'fails with unknown target types' do
+    end
+  end
+
+  context '#to_s' do
+    it 'fails with unknown target types' do
+      executable_builder.should_receive(:target_type).any_number_of_times.and_return(:foo)
+
+      expect {
+        Rake::Builder::Presenters::Makefile::BuilderPresenter.new(executable_builder)
+      }.to raise_error(RuntimeError, /Unknown.*?target type/)
+    end
+
+    context 'target' do
+      context 'for executable' do
+        it 'is executable' do
+          expect(executable_subject.to_s).to include("EXECUTABLE_TARGET = #{executable_target}")
+        end
+      end
+
+      context 'for library' do
+        it 'is a library' do
+          expect(static_library_subject.to_s).to include("LIB_TARGET = #{static_library_target}")
+        end
+      end
+    end
+
+    context 'variables' do
+      [
+        ['COMPILER', compiler],
+        ['COMPILER_FLAGS', compiler_flags],
+        ['LINKER', linker],
+        ['LINK_FLAGS', link_flags],
+        ['OBJECT_DIR', objects_path],
+      ].each do |name, value|
+        it "declares '#{name}'" do
+          expect(executable_subject.to_s).to match(/#{name}\s+=\s+#{value}/)
+        end
+      end
+
+      it "declares 'OBJECTS'" do
+        expect(executable_subject.to_s).to match(%r(OBJECTS\s+=\s+\$\(OBJECT_DIR\)/one\.o \$\(OBJECT_DIR\)/two\.o))
+      end
+    end
+
+    context 'actions' do
+      context 'for executable' do
+        it 'builds the target' do
+          expected = <<EOT
+$(EXECUTABLE_TARGET): $(OBJECTS)
+	$(LINKER) $(LINK_FLAGS) -o $(EXECUTABLE_TARGET) $(OBJECTS)
+EOT
+          expect(executable_subject.to_s).to include(expected)
+        end
+      end
+      context 'for static library' do
+        it 'builds the target' do
+          expected = <<EOT
+	rm -f $(LIB_TARGET)
+	ar -cq $(LIB_TARGET) $(OBJECTS)
+	ranlib $(LIB_TARGET)
+EOT
+          expect(static_library_subject.to_s).to include(expected)
+        end
+      end
+      context 'for shared library' do
+        it 'builds the target' do
+        end
+      end
+    end
+  end
+
+  context '#save' do
+    it 'creates the makefile' do
+      File.should_receive(:open).with(makefile_name, 'w')
+
+      executable_subject.save
+    end
+
+    it 'saves' do
+      file = stub('File')
+      File.stub(:open).with(makefile_name, 'w') do |&block|
+        block.call file
+      end
+
+      file.should_receive(:write).with(/COMPILER.*?= the_compiler/)
+
+      executable_subject.save
+    end
+  end
+end
+
