6a46b9073ff113eb87f2a25e85118c361021dc4b
Joe Yates <joe.g.yates@gmail.com>
2013-01-26 18:52:27 +0100

    Stub LocalConfig specs

--- spec/unit/rake/builder/local_config_spec.rb
+++ spec/unit/rake/builder/local_config_spec.rb
@@ -1,69 +1,52 @@
 require 'spec_helper'
 
-LOCAL_CONFIG_SPEC_PATH = File.expand_path( File.dirname(__FILE__) )
-
 describe Rake::Builder::LocalConfig do
-  include RakeBuilderHelper
-
-  before do
-    @local_config_file = Rake::Path.expand_with_root( '.rake-builder', LOCAL_CONFIG_SPEC_PATH )
-    @expected_path     = "/some/special/path"
-    @config            = {:rake_builder=>{:config_file=>{:version=>"1.0"}}, :include_paths=>[ @expected_path ]}
+  let(:local_config_file) { 'local_config' }
+  let(:include_paths) { ['/foo/bar'] }
+  let(:compilation_options) { ['foo', 'bar'] }
+  let(:config_data) do
+    {
+      :rake_builder => {
+        :config_file => {:version => '1.1'},
+      },
+      :include_paths => include_paths,
+      :compilation_options => compilation_options,
+    }
+  end
+  let(:bad_config_data) do
+    config_data[:rake_builder][:config_file][:version] = '0.1'
+    config_data
   end
 
-  after do
-    `rm -f '#{ @local_config_file }'`
-  end
+  before { YAML.stub(:load_file).and_return(config_data) }
 
-  it 'works if there\'s no config file' do
-    lambda do
-      @builder = cpp_builder( :executable )
-    end.should_not raise_error
-  end
+  subject { Rake::Builder::LocalConfig.new(local_config_file) }
 
-  it 'loads config files' do
-    save_config
-    config = Rake::Builder::LocalConfig.new( @local_config_file )
-    config.load
+  context '#load' do
+    it 'loads the file' do
+      YAML.should_receive(:load_file).and_return(config_data)
 
-    config.include_paths.     should         include( @expected_path )
-  end
-
-  it 'fails if the file version is incorrect' do
-    @config[ :rake_builder ][ :config_file ].delete( :version )
-    save_config
-    lambda do
-      config = Rake::Builder::LocalConfig.new( @local_config_file )
-      config.load
-    end.should raise_error(Rake::Builder::Error, 'Config file version incorrect')
-  end
-
-  context 'dependencies' do
-    before do
-      Rake::Task.clear
-      @project = cpp_builder( :executable )
-      Rake::Task[ 'clean' ].execute
-      `rm -f #{ @project.local_config }`
+      subject.load
     end
 
-    context 'when local_config is invoked' do
-      it 'should no longer be needed' do
-        File.exist?( @project.local_config ).should be_false
-        Rake::Task[ @project.local_config ].needed?.should be_true
+    it 'fails if the version is not recognized' do
+      YAML.should_receive(:load_file).and_return(bad_config_data)
 
-        Rake::Task[ @project.local_config ].invoke
-
-        File.exist?( @project.local_config ).should be_true
-        Rake::Task[ @project.local_config ].needed?.should be_false
-      end
+      expect {
+        subject.load
+      }.to raise_error(Rake::Builder::Error, /version incorrect/)
     end
-  end
 
-  private
+    it 'sets the include_paths' do
+      subject.load
 
-  def save_config(config = @config, filename = @local_config_file)
-    File.open(filename, 'w') do |file|
-      file.write config.to_yaml
+      expect(subject.include_paths).to eq(include_paths)
+    end
+
+    it 'sets compilation_options' do
+      subject.load
+
+      expect(subject.compilation_options).to eq(compilation_options)
     end
   end
 end
