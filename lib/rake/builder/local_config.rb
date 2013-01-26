78a226bcb421e02551440ab765303cb6ea67635e
Joe Yates <joe.g.yates@gmail.com>
2013-01-26 18:51:53 +0100

    Whitespace

--- lib/rake/builder/local_config.rb
+++ lib/rake/builder/local_config.rb
@@ -7,33 +7,35 @@ class Rake::Builder
     attr_accessor :include_paths
     attr_accessor :compilation_options
 
-    def initialize( file_name )
+    def initialize(file_name)
       @file_name           = file_name
       @include_paths       = []
       @compilation_options = []
     end
 
     def load
-      config = YAML.load_file( @file_name )
+      config = YAML.load_file(@file_name)
 
-      version             = config[:rake_builder][:config_file][:version]
-      @include_paths       = config[:include_paths]
-      @compilation_options = config[:compilation_options]
-      if not VERSIONS.find_index(version)
+      version              = config[:rake_builder][:config_file][:version]
+      if not VERSIONS.include?(version)
         raise Rake::Builder::Error.new('Config file version incorrect') 
       end
+      @include_paths       = config[:include_paths]
+      @compilation_options = config[:compilation_options]
     end
 
     def save
-      File.open( @file_name, 'w' ) do | file |
+      File.open(@file_name, 'w') do |file|
         file.write config.to_yaml
       end
     end
 
     def config
-      { :rake_builder        => { :config_file => { :version => VERSIONS[-1] } },
+      {
+        :rake_builder        => {:config_file => {:version => VERSIONS[-1]}},
         :include_paths       => @include_paths,
-        :compilation_options => @compilation_options }
+        :compilation_options => @compilation_options
+      }
     end
   end
 end
