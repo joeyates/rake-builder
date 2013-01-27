1671e147c2eeb2ee0ec8e51d33c629dfc4caf0c5
Joe Yates <joe.g.yates@gmail.com>
2013-01-28 00:43:30 +0100

    Whitespace

--- lib/rake/once_task.rb
+++ lib/rake/once_task.rb
@@ -2,15 +2,13 @@ require 'rubygems' if RUBY_VERSION < '1.9'
 require 'rake/tasklib'
 
 module Rake
-
   # A task whick is no longer needed after its first invocation
   class OnceTask < Task
-
     attr_accessor :invoked
     attr_accessor :timestamp
 
-    def self.define_task( *args, &block )
-      task = super( *args, &block )
+    def self.define_task(*args, &block)
+      task = super(*args, &block)
       task.timestamp = nil
       task.invoked = false
       task
@@ -25,9 +23,7 @@ module Rake
     def needed?
       ! @invoked
     end
-
   end
-
 end
 
 def once_task(*args, &block)
